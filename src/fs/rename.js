8e1aa604f198be2c547aaf0c2ef73dd8d5344b73
Natalia Popova <na.popova@qiwi.com>
2022-06-04 10:32:20 +0200

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,19 @@
+import {rename as renameFs, stat} from 'fs'
+
 export const rename = async () => {
-    // Write your code here 
+    const error = 'FS operation failed'
+
+    await stat('src/fs/files/properFilename.md', async (err, stats) => {
+        if (!stats) {
+            await renameFs('src/fs/files/wrongFilename.txt',
+                'src/fs/files/properFilename.md',
+                (err) => {
+                if (err) throw new Error(error);
+            });
+        } else {
+            throw new Error(error)
+        }
+    })
 };
+
+await rename()
