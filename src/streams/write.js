2aeac49e080103f3ade6534b83892d4fcbb511be
Natalia Popova <na.popova@qiwi.com>
2022-06-04 23:21:56 +0200

    feat: add streams-write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,19 @@
+import { fileURLToPath } from "url";
+import path from "path";
+import { createWriteStream } from "fs";
+
 export const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const writeStream = await createWriteStream(path.join(__dirname, '/files/fileToWrite.txt'),
+        {encoding: 'utf8', flags: 'a'})
+
+    process.stdin.on('data', async (data) => {
+        writeStream.write(data)
+        process.exit();
+    })
+
 };
+
+write()
