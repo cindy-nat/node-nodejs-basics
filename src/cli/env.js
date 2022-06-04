60ab347c358bafeff6329857104880103d1873d4
Natalia Popova <na.popova@qiwi.com>
2022-06-04 11:50:25 +0200

    feat: add cli-parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+    const values = process.env
+    const valueKeys = Object.entries(values)
+    const rssValues = valueKeys.filter(value => value[0].startsWith('RSS_'))
+
+    rssValues.forEach(rssValue => console.log(`${rssValue[0]}=${rssValue[1]};`))
 };
+
+parseEnv()
+
+// RSS_name1=value1 RSS_name2=value2 node src/cli/env.js
