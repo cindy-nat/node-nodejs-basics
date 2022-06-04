a8521cc13c804cfcf5d5d47cd9300d0c01c1ba84
Natalia Popova <na.popova@qiwi.com>
2022-06-04 10:51:16 +0200

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,10 @@
+import {readFile} from 'fs'
+
 export const read = async () => {
-    // Write your code here 
+    await readFile('src/fs/files/fileToRead.txt','utf8',(err, data) => {
+        if (err) throw new Error('FS operation failed')
+        console.log(data)
+    })
 };
+
+read()
