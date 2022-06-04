dabe1328d698e80c09f4bcf5bb1248c8f12835dd
Natalia Popova <na.popova@qiwi.com>
2022-06-04 09:52:29 +0200

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,30 @@
+import {
+    copyFile,
+    constants,
+    mkdir,
+    readdir
+} from 'fs'
+import * as path from "path";
+
 export const copy = async () => {
-    // Write your code here 
+    const oldPath = 'src/fs/files'
+    const newPath = 'src/fs/files_copy'
+    const errText = 'FS operation failed'
+    await mkdir(newPath, (err) => {
+        if (err) throw new Error(errText);
+    })
+    await readdir(oldPath, (err, files) => {
+        if (err) {
+            throw new Error(errText);
+        } else {
+            files.forEach(async (file) => {
+                await copyFile(path.join(oldPath, file), path.join(newPath, file), constants.COPYFILE_EXCL,
+                    (err) => {
+                        if (err) throw new Error(errText)
+                    });
+            })
+        }
+    })
 };
+
+await copy()
