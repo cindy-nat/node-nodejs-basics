9a9f81c69ef05a330bb262355618225a96653206
Natalia Popova <na.popova@qiwi.com>
2022-06-04 17:44:52 +0200

    feat: add streams-read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,27 @@
+import * as path from "path";
+import { createReadStream } from 'fs'
+import { fileURLToPath } from "url";
+
 export const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    try {
+        let data = ''
+        const readerStream = await createReadStream(path.join(__dirname, '/files/fileToRead.txt'),
+            {encoding: 'utf8'})
+
+        readerStream.on('data', (chunk) =>  {
+            data += chunk;
+        });
+
+        readerStream.on('end', () => {
+            process.stdout.write(data+ '\n')
+        })
+    }
+    catch (err) {
+        throw new Error('FS operation failed')
+    }
 };
+
+read()
