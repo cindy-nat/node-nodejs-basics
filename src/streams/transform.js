c5fb6183a1161cf1c72a22596b02bee52791b110
Natalia Popova <na.popova@qiwi.com>
2022-06-05 00:12:11 +0200

    feat: add streams-transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,22 @@
+import {Transform} from "stream";
+
 export const transform = async () => {
-    // Write your code here 
+
+    const reversed = new Transform({
+        transform(chunk, encoding, done) {
+            const result = chunk.split('').reverse().join('');
+            done(null, result);
+        }
+    });
+
+    process.stdin.on('data', async (data) => {
+        await reversed._transform(data.toString(),'utf-8', (error, result) => {
+            if (error) throw new Error('Reverse error')
+            process.stdout.write(result+ '\n')
+        })
+        process.exit();
+    })
+
 };
+
+transform()
