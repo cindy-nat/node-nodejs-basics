8e38896a5975c80ce85d7352b82e995264c14e77
Natalia Popova <na.popova@qiwi.com>
2022-06-05 00:22:28 +0200

    feat: add zip-compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,17 @@
+import {createGzip} from 'zlib'
+import {createReadStream, createWriteStream} from "fs";
+import {fileURLToPath} from "url";
+import path from "path";
+
 export const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const griz = await createGzip();
+
+    const readStream = await createReadStream(path.join(__dirname, '/files/fileToCompress.txt'));
+    const writeStream = await createWriteStream(path.join(__dirname, '/files/archive.gz'));
+    readStream.pipe(griz).pipe(writeStream);
 };
+
+compress()
