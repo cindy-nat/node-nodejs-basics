8c8836f5f8aec257114fb6ed4d1208d4b3e3e810
Natalia Popova <na.popova@qiwi.com>
2022-06-04 09:31:04 +0200

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,9 @@
+import { writeFile } from "fs";
+
 export const create = async () => {
-    // Write your code here 
+    await writeFile("src/fs/files/fresh.txt", "I am fresh and young", { flag: "wx" }, (err) => {
+        if (err) throw new Error('FS operation failed');
+    })
 };
+
+await create()
