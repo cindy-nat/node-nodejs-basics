a0e2b61b5edf5805d72ff1299e11712e80ab7088
Natalia Popova <na.popova@qiwi.com>
2022-06-04 11:20:03 +0200

    feat: add cli-args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const propName = '--propName';
+    const prop2Name = '--prop2Name'
+    const processArguments = process.argv
+    const foundIndexPropName = processArguments.indexOf(propName)
+    const foundIndexProp2Name = processArguments.indexOf(prop2Name)
+
+    console.log(`propName is ${processArguments[foundIndexPropName+1]}, prop2Name is ${processArguments[foundIndexProp2Name+1]}`)
 };
+
+parseArgs()
+
+// node src/cli/args.js --propName value --prop2Name value2
