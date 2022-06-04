54c3e2059f377b0d21db7a4018ac150461bd2580
Natalia Popova <na.popova@qiwi.com>
2022-06-04 17:11:38 +0200

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import {readFile} from "fs/promises";
+import * as path from "path";
+import {fileURLToPath} from "url";
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const { createHash } = await import('node:crypto');
+
+    const hash = createHash('sha256');
+    try {
+        const data = await readFile(path.join(__dirname, 'files/fileToCalculateHashFor.txt'), 'utf-8')
+        hash.update(data);
+        const hex = hash.digest('hex')
+        console.log(hex)
+    }
+    catch (err) {
+        throw new Error('FS operation failed')
+    }
+
 };
+
+calculateHash()
