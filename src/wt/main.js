19ad3a9dabdcd18dc53e88e8d2162d7be0bddc8e
Natalia Popova <na.popova@qiwi.com>
2022-06-05 19:54:44 +0200

    feat: add wt functions in main and worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,31 @@
+import {cpus} from 'os'
+import {Worker} from "node:worker_threads";
+import {fileURLToPath} from "url";
+import path from "path";
+
 export const performCalculations = async () => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const coresQ = cpus().length
+
+    let workers = []
+
+    for (let i = 0; i < coresQ; i++) {
+        workers.push(new Promise(async (resolve, reject) => {
+            const worker = new Worker(path.join(__dirname, "./worker.js"), {workerData: {n: 10+i}});
+            try {
+                await worker.on('message', resolve);
+            }
+            catch (err) {
+                worker.on('error', err);
+            }
+            worker.on('exit', reject)
+        }))
+    }
+
+    await Promise.all(workers).then((result) =>
+        result.forEach(data =>
+            console.log({status: data ? 'resolved' : 'error', data: data})))
 };
+
+performCalculations()
