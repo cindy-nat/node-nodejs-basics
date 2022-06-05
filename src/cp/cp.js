4b4290ef113b1c86e65de23e8e1871f2301c9605
Natalia Popova <na.popova@qiwi.com>
2022-06-05 21:38:15 +0200

    feat: add cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import {fork} from 'child_process'
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const child = fork(path.join(__dirname, 'files/script.js'), args);
+
+    child.on('error', (error)=> {throw new Error(error.message)})
+
 };
+
+spawnChildProcess(['done'])
